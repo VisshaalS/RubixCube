352e8ea315fe8364ff42ce81ffb67d934065d889
Phill <phillipq99@gmail.com>
2018-10-27 12:51:14 -0400

    Added code to save image.

--- app/src/main/java/com/example/solverino/MainActivity.java
+++ app/src/main/java/com/example/solverino/MainActivity.java
@@ -3,7 +3,10 @@ package com.example.solverino;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -11,11 +14,13 @@ import android.widget.Button;
 import android.widget.ImageView;
 
 import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     Button bPic;
     ImageView imageResult;
-    File myFilesDir = getExternalFilesDir(null);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,24 +39,30 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View v) {
         if (v == bPic) {
-            Intent takePicture = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-            startActivity(takePicture);
-
-        }
-    }
-
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == 0) {
-            try {
-                Bitmap cameraBitmap;
-                cameraBitmap = BitmapFactory.decodeFile(myFilesDir + "/temp.jpg");
-                Bitmap.createBitmap(cameraBitmap);
-                imageResult.setImageBitmap(cameraBitmap);
-            } catch (Exception e) {
-                e.printStackTrace();
+            File photoFile = null;
+            try{
+                String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+                String imageFileName = "JPEG_" + timeStamp + "_";
+                File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+                photoFile = File.createTempFile(imageFileName,".jpg", storageDir);
+                // Save a file: path for use with ACTION_VIEW intents
+            } catch(IOException ex){
+                // Error occurred while trying to save the file
             }
-        }
 
+            if(photoFile != null){
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "com.example.android.fileprovider",
+                        photoFile);
+                Intent takePicture = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                takePicture.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivity(takePicture);
+            }
+
+
+        }
     }
+
+
+
 }
