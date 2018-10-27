74a538f84952ca6ad04a4a1121ea52415a5a1f69
VisshaalS <40528586+VisshaalS@users.noreply.github.com>
2018-10-27 11:03:46 -0400

    Button that opens up the camera

--- app/src/main/java/com/example/solverino/MainActivity.java
+++ app/src/main/java/com/example/solverino/MainActivity.java
@@ -1,11 +1,13 @@
 package com.example.solverino;
 
 import android.content.Intent;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
     Button bPic;
 
     @Override
@@ -15,9 +17,20 @@ public class MainActivity extends AppCompatActivity {
 
         bPic = findViewById(R.id.bPic);
 
+        bPic.setOnClickListener(this);
 
 
 
 
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v == bPic){
+            Intent takePicture = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            startActivity(takePicture);
+
+        }
     }
 }
