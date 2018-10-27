ad1ac402157eeb6d29d98236a7bbe342ec256b9c
Phill <phillipq99@gmail.com>
2018-10-27 11:30:50 -0400

    Attempted to add image saving in a temp file.

--- app/src/main/java/com/example/solverino/MainActivity.java
+++ app/src/main/java/com/example/solverino/MainActivity.java
@@ -1,14 +1,21 @@
 package com.example.solverino;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+import java.io.File;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     Button bPic;
+    ImageView imageResult;
+    File myFilesDir = getExternalFilesDir(null);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,18 +26,32 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         bPic.setOnClickListener(this);
 
-
-
+        imageResult = findViewById(R.id.imageResult);
 
 
     }
 
     @Override
     public void onClick(View v) {
-        if(v == bPic){
+        if (v == bPic) {
             Intent takePicture = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
             startActivity(takePicture);
 
         }
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 0) {
+            try {
+                Bitmap cameraBitmap;
+                cameraBitmap = BitmapFactory.decodeFile(myFilesDir + "/temp.jpg");
+                Bitmap.createBitmap(cameraBitmap);
+                imageResult.setImageBitmap(cameraBitmap);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
     }
 }
