53bcc26e6eb161330a221489a878fd8ae0fb1850
VisshaalS <40528586+VisshaalS@users.noreply.github.com>
2018-10-27 10:46:04 -0400

    Commiting for the first time

--- app/src/main/java/com/example/solverino/MainActivity.java
+++ app/src/main/java/com/example/solverino/MainActivity.java
@@ -1,13 +1,23 @@
 package com.example.solverino;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button bPic;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bPic = findViewById(R.id.bPic);
+     
+
+
+
+
     }
 }
